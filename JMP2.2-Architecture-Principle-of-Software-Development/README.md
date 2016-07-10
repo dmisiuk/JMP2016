73c5c88e420e4d45b0c389b8ff3303fa79078e5b
Dzmitry Misiuk <minsler.by@gmail.com>
2016-07-10 23:28:28 +0300

    Added SRP

---
+++ JMP2.2-Architecture-Principle-of-Software-Development/README.md
@@ -0,0 +1,8 @@
+***SOLID principles***
+
+*S: Single responsibility principle*
+ 
+    It's violated in Product clas because it includes not only encapsulating fields
+    but also logic related validation and delivering product
+ 
+ 
---
+++ JMP2.2-Architecture-Principle-of-Software-Development/src/main/java/Product.java
@@ -0,0 +1,70 @@
+/**
+ * @author Dzmitry Misiuk
+ */
+public class Product {
+    private int price;
+    private String name;
+    private int width;
+    private int length;
+    private int height;
+
+    public Product(int price, String name, int width, int length, int height) {
+        this.price = price;
+        this.name = name;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
+    public boolean isValid() {
+        return price > 1 && name != null && !name.trim().isEmpty();
+    }
+
+    public void deliver(String address) {
+        // some logic to deliver product
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getVolume() {
+        return width * height * length;
+    }
+}
